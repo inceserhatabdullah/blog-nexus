8de0376b3c4b507a0009e92f52f277509ae2ba66
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-24 20:53:26 +0300

    env file enabled for app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }
+  ));
+
+  await app.listen(process.env.API_PORT ?? 3000);
+
+  const logger = new Logger('Bootstrap');
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+bootstrap().catch(error => {
+  console.error(`Failed to bootstrap application: ${String(error)}`);
+  process.exit(1);
+});
