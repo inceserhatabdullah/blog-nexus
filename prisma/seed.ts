33eaa2f29471c7f982dd9f5c38fc66984dcd124e
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-25 13:30:28 +0300

    seet ts added for migration authorities

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import { Authorities } from '../src/core/enums/authorities.enum';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const prisma = new PrismaClient({ adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }) });
+
+async function main() {
+
+  for (const authority of Object.values(Authorities)) {
+    await prisma.role.upsert(
+      {
+        where: {
+          name: authority
+        },
+        create: {
+          name: authority
+        },
+        update: {}
+      }
+    );
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
