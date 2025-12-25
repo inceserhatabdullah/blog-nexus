2b57016dffbb578e116e8d9273895d71cda41ea3
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-25 22:14:09 +0300

    auth controller updated

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, Body, HttpCode, HttpStatus } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { SignupRequestDTO } from "./dto/signup.request.dto";
+import { JwtAuthService } from "@core/auth/jwt/jwt.auth.service";
+import { SignupResponseDTO } from "./dto/signup.response.dto";
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtAuthService: JwtAuthService
+  ) { }
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  async signup(@Body() dto: SignupRequestDTO): Promise<SignupResponseDTO> {
+    const user = await this.authService.signup(dto);
+    return this.jwtAuthService.signAsync(user);
+  }
+}
