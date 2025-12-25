7c13f342d8df8dc28492ecdf6ef8e23c4fdb14c2
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-25 18:41:42 +0300

    database on init and on destroy functions init and logger support

--- src/core/database/prisma.service.ts
+++ src/core/database/prisma.service.ts
@@ -1,13 +1,53 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException, OnModuleDestroy } from "@nestjs/common";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@core/prisma/client";
+import { OnModuleInit } from "@nestjs/common";
+import { Logger } from "@nestjs/common";
+import pg from "pg";
+import { AppConfig } from "@core/app/config";
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private logger = new Logger(PrismaService.name);
+  private pool: pg.Pool;
+
   constructor() {
-    super({
-      adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
-    })
+
+    const pool = new pg.Pool(
+      {
+        connectionString: AppConfig.databaseUrl,
+      }
+    );
+
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+
+    this.pool = pool;
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      await this.$queryRaw`SELECT 1`;
+
+      this.logger.log('Database connection established successfully.');
+    } catch (e) {
+      this.logger.error('Database connection failed:', e);
+      throw new InternalServerErrorException('Database initialization failed.');
+    }
+  }
+
+  async onModuleDestroy() {
+    try {
+      await this.$disconnect();
+      await this.pool.end();
+
+      this.logger.warn('Database connection closed successfully.');
+      this.logger.warn('Database connection pool closed gracefully.');
+    } catch (e) {
+      this.logger.error('Error during database shutdown:', e);
+    }
   }
 
 }
