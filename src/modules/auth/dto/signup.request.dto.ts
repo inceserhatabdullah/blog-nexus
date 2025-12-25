3f0044b0e51796020d2d435ed2cc88e4d9013146
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-25 22:14:23 +0300

    signup dto naming change

--- src/modules/auth/dto/signup.request.dto.ts
+++ src/modules/auth/dto/signup.request.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
-export class SignupDTO {
+export class SignupRequestDTO {
   @IsString()
   @IsEmail({}, { message: 'Email is not valid.' })
   @IsNotEmpty({ message: 'Email is required.' })
