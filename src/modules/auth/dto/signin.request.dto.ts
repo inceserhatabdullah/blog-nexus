7016348ac9fe073f8499771f0856ad4f2ece66b7
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-27 13:19:18 +0300

    dto name changed

--- src/modules/auth/dto/signin.request.dto.ts
+++ src/modules/auth/dto/signin.request.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
-export class LoginDTO {
+export class SigninRequestDTO {
   @IsString()
   @IsEmail({}, { message: 'Email is not valid.' })
   @IsNotEmpty({ message: 'Email is required.' })
