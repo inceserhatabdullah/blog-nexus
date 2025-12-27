4d7a51f3e889d710d9e49fb11fda1d3d099705f6
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-27 17:30:42 +0300

    role controller module service init

---
+++ src/modules/role/role.service.ts
@@ -0,0 +1,24 @@
+import { PrismaService } from "@core/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
+import { RoleWithUserRoles } from "./types/role.type";
+import { Role } from "@core/prisma/generated/client";
+
+@Injectable()
+export class RoleService {
+
+  constructor(
+    private readonly prisma: PrismaService,
+  ) { }
+
+  async findAll(): Promise<Role[]> {
+    return this.prisma.role.findMany();
+  }
+
+  async findAllWithUserRoles(): Promise<RoleWithUserRoles[]> {
+    return this.prisma.role.findMany({
+      include: {
+        userRoles: true
+      }
+    });
+  }
+}
