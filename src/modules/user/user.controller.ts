56820eaeb501fee48c60d19be416792b0e1c8de2
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-27 14:57:56 +0300

    profile api

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, UseGuards, UseInterceptors, ClassSerializerInterceptor} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { ActiveUser } from "@core/auth/decorators/active.user.decorator";
+import type { JWTPayloadType } from "@core/auth/jwt/types/jwt.payload.type";
+import { UserService } from "./user.service";
+import { UserEntity } from "./entities/user.entity";
 
-@Controller()
-export class UserController {}
+@Controller('user')
+export class UserController {
+  constructor(
+    private readonly userService: UserService
+  ) {}
+
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(ClassSerializerInterceptor)
+  @HttpCode(HttpStatus.OK)
+  async profile(@ActiveUser() activeUser: JWTPayloadType) {
+    const user = await this.userService.findOneWithRolesAndRoles({ id: activeUser.sub });
+    return new UserEntity(user);
+  }
+}
