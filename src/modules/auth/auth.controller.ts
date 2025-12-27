86d9ed3a396cd1e6051452e21126a30edaeb6b94
inceserhatabdullah <inceserhatabdullah@gmail.com>
2025-12-27 13:44:55 +0300

    signin endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,7 +3,9 @@ import { AuthService } from "./auth.service";
 import { SignupRequestDTO } from "./dto/signup.request.dto";
 import { JwtAuthService } from "@core/auth/jwt/jwt.auth.service";
 import { SignupResponseDTO } from "./dto/signup.response.dto";
-
+import { SigninRequestDTO } from "./dto/signin.request.dto";
+import { SigninResponseDTO } from "./dto/signin.response.dto";
+  
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -15,6 +17,13 @@ export class AuthController {
   @HttpCode(HttpStatus.CREATED)
   async signup(@Body() dto: SignupRequestDTO): Promise<SignupResponseDTO> {
     const user = await this.authService.signup(dto);
+    return this.jwtAuthService.signAsync(user);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  async signin(@Body() dto: SigninRequestDTO): Promise<SigninResponseDTO> {
+    const user = await this.authService.signin(dto);
     return this.jwtAuthService.signAsync(user);
   }
 }
